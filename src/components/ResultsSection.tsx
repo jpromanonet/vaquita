02b78f7ee66a30aa851fccd461aff430c408f8b1
jpromanonet <programming@jpromano.net>
2023-03-03 18:41:33 -0300

    Almost done

--- src/components/ResultsSection.tsx
+++ src/components/ResultsSection.tsx
@@ -14,17 +14,17 @@ class ResultsSectionRaw extends React.Component<IResultsSectionProps> {
         const { result } = this.props;
 
         return (<div>
-            <h3><small>People count: {result.peopleCount}</small></h3>
-            <h3><small>Total products value: {result.totalValue.toFixed(2)} $</small></h3>
-            <h3><small>Cost per person: {result.costPerPerson.toFixed(2)} $/person</small></h3>
+            <h3><small>Cantidad de productos: {result.peopleCount}</small></h3>
+            <h3><small>Valor total de productos: {result.totalValue.toFixed(2)} $</small></h3>
+            <h3><small>Costo por persona: {result.costPerPerson.toFixed(2)} $/person</small></h3>
 
             <table className="table table-striped">
-                <caption>Transaction list</caption>
+                <caption>Lista de transacciones</caption>
                 <thead>
                 <tr>
-                    <th>Who gives</th>
-                    <th>To whom</th>
-                    <th>How much</th>
+                    <th>¿Quién paga?</th>
+                    <th>¿A quién?</th>
+                    <th>¿Cuanto?</th>
                 </tr>
                 </thead>
                 <tbody>
