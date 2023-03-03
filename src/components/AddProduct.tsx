4e284d73cf634ccb0f43bb25d89f707a2305065a
jpromanonet <programming@jpromano.net>
2023-03-03 15:20:45 -0300

    App update

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -23,7 +23,7 @@ class AddProductRaw extends React.Component<IAddProductProps> {
     }
 
     private renderInfoMessage() {
-        return <p>No product owner is selected! Please select a person first to add product!</p>;
+        return <p>¡No tenes productos asociados a une persona!, ¡por favor, asocia un producto primero!</p>;
     }
 
     private handleSubmit(name: string, price: number) {
